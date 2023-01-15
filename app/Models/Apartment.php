831189f3d54500ac4cb8a82ceae5215c2323cd28
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-01-15 17:02:22 +0400

    feat: Add soft delete column for apartments table.

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Apartment extends Model
 {
     use HasFactory;
+    use SoftDeletes;
 
     protected $fillable = [
         'beds', 'baths', 'area', 'city', 'code', 'street', 'street_nr', 'price',
