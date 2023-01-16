25c38d577794595b2488d9764e98fee0a873568b
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-01-16 13:55:12 +0400

    feat: Add component Input with label.

---
+++ resources/js/Components/UI/InputWithLabel.vue
@@ -0,0 +1,16 @@
+<template>
+  <label class="label">{{ label }}</label>
+  <input :value="modelValue" type="text" class="input" @input="$emit('update:modelValue', $event.target.value)" />
+  <div v-if="error" class="error"> {{ error }}</div>
+</template>
+
+<script setup>
+defineProps({
+    modelValue: {type: [String, Number, null], required: true},
+    modelModifiers: { default: () => ({}) },
+    label: {type: String, required: true},
+    error: {type: [String]},
+})
+
+defineEmits(['update:modelValue'])
+</script>
--- resources/js/Pages/Apartment/Create.vue
+++ resources/js/Pages/Apartment/Create.vue
@@ -2,44 +2,28 @@
   <form @submit.prevent="create">
     <div class="grid grid-cols-6 gap-4">
       <div class="col-span-2">
-        <label class="label">Beds</label>
-        <input v-model.number="form.beds" type="text" class="input" />
-        <div v-if="form.errors.beds" class="error"> {{ form.errors.beds }}</div>
+        <InputWithLabel v-model.number="form.beds" label="Beds" :error="form.errors.beds" />
       </div>
       <div class="col-span-2">
-        <label class="label">Baths</label>
-        <input v-model.number="form.baths" type="text" class="input" />
-        <div v-if="form.errors.baths" class="error"> {{ form.errors.baths }}</div>
+        <InputWithLabel v-model.number="form.baths" label="Baths" :error="form.errors.baths" />
       </div>
       <div class="col-span-2">
-        <label class="label">Area</label>
-        <input v-model.number="form.area" type="text" class="input" />
-        <div v-if="form.errors.area" class="error"> {{ form.errors.area }}</div>
+        <InputWithLabel v-model.number="form.area" label="Area" :error="form.errors.area" />
       </div>
       <div class="col-span-2">
-        <label class="label">City</label>
-        <input v-model.trim="form.city" type="text" class="input" />
-        <div v-if="form.errors.city" class="error"> {{ form.errors.city }}</div>
+        <InputWithLabel v-model.trim="form.city" label="City" :error="form.errors.city" />
       </div>
       <div class="col-span-2">
-        <label class="label">Street</label>
-        <input v-model.trim="form.street" type="text" class="input" />
-        <div v-if="form.errors.street" class="error"> {{ form.errors.street }}</div>
+        <InputWithLabel v-model.trim="form.street" label="Street" :error="form.errors.street" />
       </div>
       <div class="col-span-2">
-        <label class="label">Postal code</label>
-        <input v-model.trim="form.code" type="text" class="input" />
-        <div v-if="form.errors.code" class="error"> {{ form.errors.code }}</div>
+        <InputWithLabel v-model.trim="form.code" label="Postal code" :error="form.errors.code" />
       </div>
       <div class="col-span-2">
-        <label class="label">Street number</label>
-        <input v-model.number="form.street_nr" type="text" class="input" />
-        <div v-if="form.errors.street_nr" class="error"> {{ form.errors.street_nr }}</div>
+        <InputWithLabel v-model.number="form.street_nr" label="Street number" :error="form.errors.street_nr" />
       </div>
       <div class="col-span-2">
-        <label class="label">Price</label>
-        <input v-model.number="form.price" type="text" class="input" />
-        <div v-if="form.errors.price" class="error"> {{ form.errors.price }}</div>
+        <InputWithLabel v-model.number="form.price" label="Price" :error="form.errors.price" />
       </div>
       <div class="col-span-6">
         <button type="submit" class="button-primary">Create</button>
@@ -50,6 +34,7 @@
 
 <script setup>
 import {useForm} from '@inertiajs/inertia-vue3'
+import InputWithLabel from '@/Components/UI/InputWithLabel.vue'
 
 const form = useForm({
     beds: 1,
@@ -64,9 +49,3 @@ const form = useForm({
 
 const create = () => form.post(route('apartment.store'))
 </script>
-
-<style scoped>
-.error {
-    color: red;
-}
-</style>
