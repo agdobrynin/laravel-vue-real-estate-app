47a789dfe0c2d11152018fc9dbd9391fb92381f7
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-01-18 12:11:04 +0400

    feat: Use FormApartment component

--- resources/js/Pages/Apartment/Create.vue
+++ resources/js/Pages/Apartment/Create.vue
@@ -1,55 +1,10 @@
 <template>
-  <form @submit.prevent="create">
-    <div class="grid grid-cols-6 gap-4">
-      <div class="col-span-2">
-        <InputWithLabel
-          v-model.number="form.beds" label="Beds" :error="form.errors.beds"
-          :maska-options="{mask: '##'}"
-        />
-      </div>
-      <div class="col-span-2">
-        <InputWithLabel
-          v-model.number="form.baths" label="Baths" :error="form.errors.baths"
-          :maska-options="{mask: '##'}"
-        />
-      </div>
-      <div class="col-span-2">
-        <InputWithLabel
-          v-model.number="form.area" label="Area" :error="form.errors.area"
-          :maska-options="{mask: '###'}"
-        />
-      </div>
-      <div class="col-span-2">
-        <InputWithLabel v-model.trim="form.city" label="City" :error="form.errors.city" />
-      </div>
-      <div class="col-span-2">
-        <InputWithLabel v-model.trim="form.street" label="Street" :error="form.errors.street" />
-      </div>
-      <div class="col-span-2">
-        <InputWithLabel
-          v-model.trim="form.code" label="Postal code" :error="form.errors.code"
-          :maska-options="{mask: '#####-####'}"
-        />
-      </div>
-      <div class="col-span-2">
-        <InputWithLabel v-model.number="form.street_nr" label="Street number" :error="form.errors.street_nr" />
-      </div>
-      <div class="col-span-2">
-        <InputWithLabel
-          v-model.number="form.price" label="Price" :error="form.errors.price"
-          :maska-options="{mask: '9 99#', returnUnmasked: true, reversed:true, tokens: {'9': {pattern: '[0-9]', repeated: true, multiple: true}}}"
-        />
-      </div>
-      <div class="col-span-6">
-        <button type="submit" class="button-primary">Create</button>
-      </div>
-    </div>
-  </form>
+  <FormApartment button-title="Create" :apartment="form" @on-submit="create" />
 </template>
 
 <script setup>
 import {useForm} from '@inertiajs/inertia-vue3'
-import InputWithLabel from '@/Components/UI/InputWithLabel.vue'
+import FormApartment from '@/Pages/Apartment/Components/FormApartment.vue'
 
 const form = useForm({
     beds: 1,
