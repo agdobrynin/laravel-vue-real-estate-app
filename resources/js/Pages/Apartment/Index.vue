72343eb78aaeb0421313c0baa5032fb494dc26e9
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-01-23 13:06:24 +0400

    fix: Show pagination if total pages more than once page

--- resources/js/Pages/Apartment/Index.vue
+++ resources/js/Pages/Apartment/Index.vue
@@ -1,12 +1,12 @@
 <template>
-  <div v-if="list.data.length" class="w-full flex justify-center mt-8 mb-8">
+  <div v-if="list.last_page > 1" class="w-full flex justify-center mt-8 mb-8">
     <Pagination :links="list.links" />
   </div>
   <div><FormApartmentFilter :filters="filters" /></div>
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
     <ApartmentItem v-for="item in list.data" :key="item.id" :item="item" />
   </div>
-  <div v-if="list.data.length" class="w-full flex justify-center mt-8 mb-8">
+  <div v-if="list.last_page > 1" class="w-full flex justify-center mt-8 mb-8">
     <Pagination :links="list.links" />
   </div>
 </template>
